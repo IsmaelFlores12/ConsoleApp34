5c43d1e80d13541832f61ef86e1b85fba57497db
Lab2-32 <Lab2-32@LAB2-32>
2025-03-13 18:36:32 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string>series = new Dictionary<int,string>();
+            series.Add(10, "breaking bad");
+            series.Add(9, " hora de aventura");
+            series.Add(8, " un show mas");
+            series.Add(7, " daredevil");
+            series.Add(6, " umbrella academy");
+            series.Add(5, "games of trones");
+            series.Add(4, " gravity falls");
+            series.Add(3, " el chavo del 8");
+            series.Add(2, "  the oficce");
+            series.Add(1, "club de cuervos");
+        }
+    }
+}
